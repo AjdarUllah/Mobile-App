f7c6dd3aacd4a303f11b8906c4341e0619e6cc16
AjdarUllah <166685549+AjdarUllah@users.noreply.github.com>
2026-05-22 17:10:36 +0300

    Simplify app build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,21 +10,12 @@ android {
         applicationId = "com.ajdar.magnetometerrecorder"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.3.1"
+        versionCode = 5
+        versionName = "0.3.2"
     }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
-    testOptions {
-        unitTests.isIncludeAndroidResources = true
-    }
-}
-
-dependencies {
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("org.robolectric:robolectric:4.13")
 }
