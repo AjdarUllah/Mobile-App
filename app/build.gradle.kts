15c027263d59b42719478f0648e1f10b34d3d370
AjdarUllah <166685549+AjdarUllah@users.noreply.github.com>
2026-05-23 14:15:57 +0300

    Bump MAG Recorder to version 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.ajdar.magnetometerrecorder"
         minSdk = 26
         targetSdk = 35
-        versionCode = 40
-        versionName = "0.4.0"
+        versionCode = 50
+        versionName = "5.0"
     }
 
     compileOptions {
