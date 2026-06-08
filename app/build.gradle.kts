524193c54d5b1711719b7a6db19a1e7a511ea7d5
AjdarUllah <166685549+AjdarUllah@users.noreply.github.com>
2026-06-08 12:51:53 +0300

    Bump app version to 6.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.ajdar.magnetometerrecorder"
         minSdk = 26
         targetSdk = 35
-        versionCode = 61
-        versionName = "6.1"
+        versionCode = 62
+        versionName = "6.2"
     }
 
     compileOptions {
